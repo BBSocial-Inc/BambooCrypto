3e28ba67616510419d4cde9e217d37e29cf6b8d6
bilyhack12 <ibrahim.bileri.ib@gmail.com>
2024-07-18 22:51:33 +0000

    fix: changed the intenal bamboo video to an embedded youtube link

--- src/components/landing/Why/Why.js
+++ src/components/landing/Why/Why.js
@@ -3,56 +3,19 @@ import "./why.scss";
 import Thumbnail from "../../../assets/images/video-thumbnail.png";
 
 const Why = () => {
-  const videoRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  const playVideo = () => {
-    if (videoRef.current) {
-      videoRef.current.volume = 1;
-      videoRef.current.play();
-      setIsPlaying(true);
-    }
-  };
-
-  const pauseVideo = () => {
-    if (videoRef.current) {
-      videoRef.current.pause();
-      setIsPlaying(false);
-    }
-  };
-
   return (
     <section className="whymain">
       <div className="whyinner">
         <h1 className="whyinnerhead">Why Bamboo Crypto? Our vision!</h1>
       </div>
       <div className="video-container">
-        <video
-          ref={videoRef}
-          className="video-element"
-          playsInline
-          loop
-          width="100%"
-          id="myVideo"
-          poster={Thumbnail}
-        >
-          <source src="/videos/bamboo.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        {!isPlaying && (
-          <button onClick={playVideo} className="play-btn">
-            <img src="/assets/playbtn.svg" alt="Play" className="btninnerimg" />
-          </button>
-        )}
-        {isPlaying && (
-          <button onClick={pauseVideo} className="pause-btn">
-            <img
-              src="/assets/pausebtn.svg"
-              alt="Pause"
-              className="btninnerimgpause"
-            />
-          </button>
-        )}
+            <iframe
+              src="https://www.youtube.com/embed/xcZKQpCXsi8"
+              title="Bamboo Crypto Community Conversation"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            >
+            </iframe>
       </div>
     </section>
   );
